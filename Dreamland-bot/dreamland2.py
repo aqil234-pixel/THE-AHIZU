6dffdb3affa19eeae34533a0b035146f0d59f77a
Hilmy Nur Iman <168150259+HilmyNurIman@users.noreply.github.com>
2026-05-12 09:17:27 +0700

    Update print statement from 'Hello' to 'Goodbye'

--- Dreamland-bot/dreamland2.py
+++ Dreamland-bot/dreamland2.py
@@ -522,6 +522,7 @@ def main():
     
     # Handlers Perintah & Tombol
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(start, pattern='^start_back$'))
     app.add_handler(CallbackQueryHandler(admin_cek_bug_callback, pattern='^admin_cek_bug$'))
     app.add_handler(CallbackQueryHandler(menu_katalog, pattern='^lihat_katalog$'))
     app.add_handler(CallbackQueryHandler(tampilkan_deskripsi, pattern='^desc_')) # Harus ada ini!
